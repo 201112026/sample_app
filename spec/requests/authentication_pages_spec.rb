b659f9bfd82882c522b9350e9ef45259229ec30c
201112026 <i201112026@cc.niit.ac.jp>
2014-06-02 15:59:51 +0900

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -51,6 +51,7 @@ describe "Authentication" do
       let(:user) { FactoryGirl.create(:user) }
 
       describe "when attempting to visit a protected page" do
+
         before do
           visit edit_user_path(user)
           fill_in "Email",    with: user.email
@@ -59,11 +60,11 @@ describe "Authentication" do
         end
 
         describe "after signing in" do
-
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
           end
         end
+
       end
 
       describe "in the Users controller" do
@@ -83,6 +84,20 @@ describe "Authentication" do
           it { should have_title('Sign in') }
         end
       end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
     end
 
     describe "as wrong user" do
